3d9e34682af08eb24d074f7a141fc09188466862
saveenr <saveenr@localhost>
2013-01-18 11:40:16 -0800

    Added more methods

---
+++ MetaWeblogSharp/PostInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace MetaWeblogSharp
+{
+    public class PostInfo
+    {
+        public string Title;
+        public string Link;
+        public System.DateTime DateCreated;
+        public string PostID;
+        public string UserID;
+        public int CommentCount;
+        public string PostStatus;
+        public string PermaLink;
+        public string Description;
+
+        public PostInfo()
+        {
+            
+        }
+    }
+}
