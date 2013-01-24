e19d412ce8ecc74c0e5b707c1a62faa5ed70d6c4
saveenr <saveenr@localhost>
2013-01-24 01:32:25 -0800

    Updated demo app

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -12,45 +12,45 @@ namespace DemoApp
             // string blogdef = "\\saveenr\\skydrive\\private\\reference\\msdn.xml";
             string blogdef = "\\saveenr\\skydrive\\private\\reference\\viziblr.xml";
 
-            var blogaccount = MetaWeblogSharp.BlogConnectionInfo.Load(blogdef);
+            var blogcon = MetaWeblogSharp.BlogConnectionInfo.Load(blogdef);
+            var client = new MetaWeblogSharp.Client(blogcon);
 
-            var service = new MetaWeblogSharp.Client(blogaccount);
-            var posts = service.GetRecentPosts(10);
+            var posts = client.GetRecentPosts(10);
 
             var first_post_1 = posts[0];
-            var first_post_2 = service.GetPost(first_post_1.PostID);
+            var first_post_2 = client.GetPost(first_post_1.PostID);
             var bytes = System.IO.File.ReadAllBytes("test1.png");
 
-            var mo = service.NewMediaObject("8901234567890.png", "image/png", bytes);
+            var mo = client.NewMediaObject("8901234567890.png", "image/png", bytes);
 
             var categories1 = new List<string>
             {
                 // "A", "B", "C"
             };
 
-            var categories2 = service.GetCategories();
+            var categories2 = client.GetCategories();
 
-            var blogs = service.GetUsersBlogs();
+            var blogs = client.GetUsersBlogs();
 
             var title1 = string.Format("Test {0}" , System.DateTime.Now.ToString());
             var description1 = string.Format("<p>Hello World</p>\n<p>{0}</p>", System.DateTime.Now);
-            var new_post_id = service.NewPost(title1, description1, categories1, false);
-            var new_post_info = service.GetPost(new_post_id);
-            var successedit1 = service.EditPost(new_post_info.PostID, title1, description1, categories1, false);
-            var new_post_info2 = service.GetPost(new_post_id);
-            var success_delete = service.DeletePost(new_post_id);
+            var new_post_id = client.NewPost(title1, description1, categories1, false);
+            var new_post_info = client.GetPost(new_post_id);
+            var successedit1 = client.EditPost(new_post_info.PostID, title1, description1, categories1, false);
+            var new_post_info2 = client.GetPost(new_post_id);
+            var success_delete = client.DeletePost(new_post_id);
 
             try
             {
                 // this will raise an exception if the post does not exist
-                var x = service.GetPost(new_post_id);
+                var x = client.GetPost(new_post_id);
             }
             catch (MetaWeblogSharp.XmlRPC.XmlRPCException)
             {
                 // do nothing
             }
 
-            var u1 = service.GetUserInfo();
+            var u1 = client.GetUserInfo();
         }
     }
 }
